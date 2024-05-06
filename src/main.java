753b5c5b4619cc149e5d62871ecab1d433b500b3
Spartikin <nicholastripodibaslis01@gmail.com>
2024-05-06 21:16:43 +0930

    Commit 2
    Added Empty book, multimedia and library classes

--- src/main.java
+++ src/main.java
@@ -1,9 +1,20 @@
 
 public class main {
+	
+	// Class for Book
+	class Book {
+	}
+	
+	// Class for Multimedia
+	class Multimedia {
+	}
+	
+	// Class for Library
+	class Library {
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
