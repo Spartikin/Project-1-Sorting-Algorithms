719e440c8bef2a4d866b9954b87aeeeb411ab68c
Spartikin <nicholastripodibaslis01@gmail.com>
2024-05-06 23:20:17 +0930

    Commit 29
    Added Multimedia Testing

--- src/mainTest.java
+++ src/mainTest.java
@@ -62,5 +62,65 @@ class mainTest {
     	main.Book book = new main.Book("title", "author", "genre", 200, 2021, 1, main.Rating.PG);
         assertEquals(main.Rating.PG, book.getRating());
     }
+    
+    @Test
+    public void testMConstructor() {
+        main.Multimedia multimedia = new main.Multimedia("title", "authorPublisher", "type", "lengthDuration", 2021, 1, main.Rating.PG);
+
+        assertEquals("title", multimedia.getTitle());
+        assertEquals("authorPublisher", multimedia.getAuthor());
+        assertEquals(1, multimedia.getEdition());
+        assertEquals(main.Rating.PG, multimedia.getRating());
+    }
+
+    @Test
+    public void testMToString() {
+        main.Multimedia multimedia = new main.Multimedia("title", "authorPublisher", "type", "lengthDuration", 2021, 1, main.Rating.PG);
+        assertEquals("title by authorPublisher (Edition: 1)", multimedia.toString());
+    }
+
+    @Test
+    public void testMHashCode() {
+        main.Multimedia multimedia1 = new main.Multimedia("title", "authorPublisher", "type", "lengthDuration", 2021, 1, main.Rating.PG);
+        main.Multimedia multimedia2 = new main.Multimedia("title", "authorPublisher", "type", "lengthDuration", 2021, 1, main.Rating.PG);
+
+        assertEquals(multimedia1.hashCode(), multimedia2.hashCode());
+    }
+
+    @Test
+    public void testMEquals() {
+        main.Multimedia multimedia1 = new main.Multimedia("title", "authorPublisher", "type", "lengthDuration", 2021, 1, main.Rating.PG);
+        main.Multimedia multimedia2 = new main.Multimedia("title", "authorPublisher", "type", "lengthDuration", 2021, 1, main.Rating.PG);
+        main.Multimedia multimedia3 = new main.Multimedia("title", "authorPublisher", "type", "lengthDuration", 2021, 2, main.Rating.PG);
+        main.Multimedia multimedia4 = new main.Multimedia("title", "authorPublisher", "type", "lengthDuration", 2022, 1, main.Rating.PG);
+
+        assertTrue(multimedia1.equals(multimedia2));
+        assertFalse(multimedia1.equals(multimedia3));
+        assertTrue(multimedia1.equals(multimedia4));
+    }
+
+    @Test
+    public void testMGetTitle() {
+        main.Multimedia multimedia = new main.Multimedia("title", "authorPublisher", "type", "lengthDuration", 2021, 1, main.Rating.PG);
+        assertEquals("title", multimedia.getTitle());
+    }
+
+    @Test
+    public void testMGetEdition() {
+        main.Multimedia multimedia = new main.Multimedia("title", "authorPublisher", "type", "lengthDuration", 2021, 1, main.Rating.PG);
+        assertEquals(1, multimedia.getEdition());
+    }
+
+    @Test
+    public void testMGetAuthor() {
+        main.Multimedia multimedia = new main.Multimedia("title", "authorPublisher", "type", "lengthDuration", 2021, 1, main.Rating.PG);
+        assertEquals("authorPublisher", multimedia.getAuthor());
+    }
+
+    @Test
+    public void testMGetRating() {
+        main.Multimedia multimedia = new main.Multimedia("title", "authorPublisher", "type", "lengthDuration", 2021, 1, main.Rating.PG);
+        assertEquals(main.Rating.PG, multimedia.getRating());
+    }
 
 }
